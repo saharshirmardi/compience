18b0408b08eb428d365091d471d2400edbbf2957
MaryHemati <maryam.hemati@ut.ac.ir>
2019-02-27 13:02:20 +0330

    with adding some lines now we can subtract numbers

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main(){
 
 
 	int result = 0;
-	bool wait_for_number = true, error = false;
+	bool wait_for_number1 = true, error = false, wait_for_number2 = true;
 	for ( std::vector<std::string>::iterator it = elements.begin(); it != elements.end(); ++it ) {
 		bool is_number = true;
 		for ( std::string::iterator cit = it->begin(); cit != it->end(); ++cit)
@@ -31,21 +31,30 @@ int main(){
 				is_number = false;
 
 		if (is_number) {
-			if (wait_for_number) {
+			if (wait_for_number1) {
 				result += std::atoi(it->c_str());
-				wait_for_number = false;
-			} else {
+				wait_for_number1 = false;	
+			}
+			else if (wait_for_number2){
+				result -= std::atoi(it->c_str());
+				wait_for_number2 = false;
+			}
+			else {
 				std::cout << "ERROR" << std::endl;
 				return -1;
 			}
 		} else {
-			if (wait_for_number) {
+			if (wait_for_number1) {
 				std::cout << "ERROR" << std::endl;
 				return -1;
 			} else {
 				if ((*it) == "+"){
-					wait_for_number = true;
-				} else {
+					wait_for_number1 = true;
+				} 
+				else if ((*it) == "-"){
+					wait_for_number2 = true;
+				}
+				else {
 					std::cout << "ERROR" << std::endl;
 					return -1;
 				}
